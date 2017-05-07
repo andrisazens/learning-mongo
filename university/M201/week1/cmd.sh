4a36bd8b256b2db3c47b57100a7237fad80127a0
Andris Azens <andris.azens@inbox.lv>
2017-05-07 17:10:09 +0300

    Materials

--- university/M201/week1/cmd.sh
+++
@@ -1,1 +0,0 @@
-mongoimport --host 127.0.0.1 --port 27017 --collection people --db m201 --file /tmp/people.json
--- university/M201/week2/understanding_explain_part_2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# use mlaunch from mtools to startup a sharded cluster
-mlaunch init --single --sharded 2
-
-
-# after sharding the collection, import some documents
-mongoimport -d m201 -c people people.json
